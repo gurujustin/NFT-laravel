34d55bd87fa18a4a6fc0ffe4c3eb298ea5bbd328
Alpha Dev <67410422+alphadev102@users.noreply.github.com>
2022-08-22 05:02:33 +1000

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
